0f7fd079a8735048f269a7f9a4a087f38c15e3a5
trhgYen <1751012086yen@ou.edu.vn>
2020-05-27 16:06:55 +0700

    update sql

--- Database/StoreProcedure.sql
+++ Database/StoreProcedure.sql
@@ -1,25 +1,22 @@
 ﻿/*
 - 26/5/2020
 */
-CREATE PROC SearchPost
-(
-	@kw nvarchar(100)
-)
+CREATE PROC SearchPost ( @kw nvarchar(100) )
 AS
 BEGIN
-with tam as (
-	select p.PostID from Posts p, FoodCategories c, Post_FoodCategory pc
-	where p.PostID = pc.PostID and c.CategoryID = pc.CategoryID and
-			PostName like N'%' + @kw + '%' or
-			CategoryName like N'%' + @kw + '%' or
-			Address like N'%' + @kw + '%'
-	group by p.PostID)
+	with tam as (
+		select p.PostID from Posts p, FoodCategories c, Post_FoodCategory pc
+		where p.PostID = pc.PostID and c.CategoryID = pc.CategoryID and
+				PostName like N'%' + @kw + '%' or
+				CategoryName like N'%' + @kw + '%' or
+				Address like N'%' + @kw + '%'
+		group by p.PostID)
 
 	select * from Posts where PostID in ( select PostID from tam)
 END
 GO
-EXEC SearchPost N'Hồ Chí Minh'
 
+GO
 ---
 CREATE PROC AddPost(@AccountId int,@PostName nvarchar(50),@Content ntext, @Address nvarchar(100), @PictureURL ntext)
 AS
